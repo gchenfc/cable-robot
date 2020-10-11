f3e3a0ae017ba35c70a64c138981aa0a316f852d
Gerry Chen <gchen328@gatech.edu>
2020-10-11 12:15:28 -0400

    CMake cleanup

--- simulation/tests/testPositioningFactorGraph.cpp
+++ simulation/tests/testPositioningFactorGraph.cpp
@@ -8,8 +8,8 @@ int main(int argc, char* argv[])
 {
 	cable_robot::PositioningFactorGraph pos;
 	pos.run();
-	//cable_robot::LQRFactorGraph lqr;
-	//lqr.run();
+	// cable_robot::LQRFactorGraph lqr;
+	// lqr.run();
 	return 1;
 
 };
