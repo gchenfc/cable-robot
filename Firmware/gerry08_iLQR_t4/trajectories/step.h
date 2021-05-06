d8528d646ccfc75d7c04ea46d03be7c33d318c16
Gerry Chen <gchen328@gatech.edu>
2021-05-05 21:57:00 -0400

    git lfs

--- Firmware/gerry08_iLQR_t4/trajectories/step.h
+++ Firmware/gerry08_iLQR_t4/trajectories/step.h
@@ -1,35 +1,3 @@
-bool painton[400];
-uint8_t colorinds[400];
-uint8_t colorpalette[2][3] = {{0,0,0}, {255,255,255}};
-float traj[400][2];
-
-#define TRAJ_SETUP
-#define TRAJ_SCALE 1.0
-#define TRAJ_LOOP
-
-void setup_traj() {
-  for (int i = 0; i < 100; i++) {
-    painton[i] = true;
-    colorinds[i] = 0;
-    traj[i][0] = 1 + (i/100.0);
-    traj[i][1] = 1.15;
-  }
-  for (int i = 100; i < 200; i++) {
-    painton[i] = false;
-    colorinds[i] = 0;
-    traj[i][0] = 2;
-    traj[i][1] = 1.15;
-  }
-  for (int i = 200; i < 300; i++) {
-    painton[i] = true;
-    colorinds[i] = 1;
-    traj[i][0] = 2 - ((i-200)/100.0);
-    traj[i][1] = 1.15;
-  }
-  for (int i = 300; i < 400; i++) {
-    painton[i] = false;
-    colorinds[i] = 1;
-    traj[i][0] = 1;
-    traj[i][1] = 1.15;
-  }
-}
+version https://git-lfs.github.com/spec/v1
+oid sha256:648ab7edef83260f28d7d2c684f5676f98a71b9c2e4111b0f55b4a6a6907e32d
+size 744
