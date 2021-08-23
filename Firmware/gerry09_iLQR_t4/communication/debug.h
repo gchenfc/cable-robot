2f6cdf3a13c6a5d7e47543dffb6988baabcbf895
Gerry Chen <gerry.chen2015@gmail.com>
2021-08-23 06:54:47 -0400

    WIP: debug info

--- Firmware/gerry09_iLQR_t4/communication/debug.h
+++ Firmware/gerry09_iLQR_t4/communication/debug.h
@@ -8,11 +8,23 @@
 
 #include <Stream.h>
 
+extern Robot robot;
+
 class Debug {
  public:
-  Debug(Stream& serial) {}
+  Debug(Stream& serial) : serial_(serial) {}
 
   // Common API
   void setup() {}
-  void update() {}
+  void update() {
+    for (int i = 0; i < 4; ++i) {
+      const Winch& winch = robot.winches.at(i);
+      serial_.printf("%d %d %.2f %.2f\t|\t", winch.error(), winch.state(),
+                    winch.len(), winch.lenDot());
+    }
+    serial_.println();
+  }
+
+ private:
+  Stream& serial_;
 };
