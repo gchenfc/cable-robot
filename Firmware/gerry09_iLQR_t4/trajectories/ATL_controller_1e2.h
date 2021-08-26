8604d6e9eb6d638962968eafe46d028d32248ce7
Gerry Chen <gerry.chen2015@gmail.com>
2021-08-26 06:25:54 -0400

    trajectories for iLQR controller

---
+++ Firmware/gerry09_iLQR_t4/trajectories/ATL_controller_1e2.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:33c3070b32066dc7d83800302ecdfb9224920105c950330cd1bca29bd3059bd4
+size 813522
---
+++ Firmware/gerry09_iLQR_t4/trajectories/ATL_controller_1e2_static.h
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:10adb0f162c19f7b6ab2ef8333633d2622256a1cb9a698deac34d9ef9022a93b
+size 813529
