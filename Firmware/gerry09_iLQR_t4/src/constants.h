89de7e24fad3d04599106e100ae0e034935fa585
Gerry Chen <gerry.chen2015@gmail.com>
2021-09-01 10:49:42 -0400

    refactor constants

--- Firmware/gerry09_iLQR_t4/src/constants.h
+++ Firmware/gerry09_iLQR_t4/src/constants.h
@@ -8,8 +8,9 @@
 #ifndef DEBUG_CONSTANTS
 
 /******** Winch Parameters **********/
-float kZeros[4] = {5.027, 6.096, 2.678, 5.733};
+float kZeros[4] = {4.868, 4.672, 6.490, 7.980};
 static constexpr float kR = 0.0254 / 2;
+static constexpr float kSprayDelay_s = 0.40;
 
 /******** Frame Geometry **********/
 // Klaus lab measurements:
@@ -18,9 +19,7 @@ static constexpr float kR = 0.0254 / 2;
 // 1: 2.99  -  2.27  (1.3288 : 1)  -  3.79
 // 2: 2.95  -  2.24  (1.3158 : 1)  -  3.76
 // 3: 2.91  -  2.20  (1.3318 : 1)  -  3.73
-static constexpr float kCarriageWidth = 0.135, kCarriageHeight = 0.17;
-// static constexpr float kWidth = 2.9475 - kCarriageWidth,
-//                        kHeight = 2.2325 - kCarriageHeight / 2;
+static constexpr float kFrameWidth = 2.9475, kFrameHeight = 2.2325;
 
 // DFL Measurements
 // 0: 3.05  -  2.26
@@ -28,7 +27,12 @@ static constexpr float kCarriageWidth = 0.135, kCarriageHeight = 0.17;
 // 2: 3.14  -  2.38
 // 3: 3.05  -  2.29
 // float kWidth = 3.09, kHeight = 2.32;
-static constexpr float kWidth = 3.05 - kCarriageWidth, kHeight = 2.32 - kCarriageHeight / 2;
+// static constexpr float kFrameWidth = 3.05, kFrameHeight = 2.32;
+
+// Constants including carriage
+static constexpr float kCarriageWidth = 0.187, kCarriageHeight = 0.122;
+static constexpr float kWidth = kFrameWidth - kCarriageWidth,
+                       kHeight = kFrameHeight - kCarriageHeight / 2;
 
 /******** Mathematical Constants **********/
 static constexpr float kTau = 2 * 3.1415926535897932384626433832795;
