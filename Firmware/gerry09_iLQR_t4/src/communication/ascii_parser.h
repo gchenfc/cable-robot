df61544f24b2ed3b13af13b942ee053762c1ef1c
Gerry Chen <gerry.chen2015@gmail.com>
2022-06-21 01:06:34 -0400

    parser unit test and variadic template parsing

--- Firmware/gerry09_iLQR_t4/src/communication/ascii_parser.h
+++ Firmware/gerry09_iLQR_t4/src/communication/ascii_parser.h
@@ -2,11 +2,22 @@
 
 namespace human_serial {
 
-bool until(char** buffer_start, char* buffer_end, char delim) {
-  *buffer_start = std::find(*buffer_start, buffer_end, delim);
+bool until(char** buffer_start, char* buffer_end, char delim = '\255') {
   if (*buffer_start == buffer_end) return false;
-  *((*buffer_start)++) =
-      0;  // null terminate the number and advance to next one
+  if (**buffer_start == delim) return false;
+  if (delim == '\n' && (std::find(*buffer_start, buffer_end, '\n') >
+                        std::find(*buffer_start, buffer_end, ','))) {
+    return false;
+  }
+  if (delim == '\255') {
+    if ((**buffer_start == '\n') || (**buffer_start == ',')) return false;
+    *buffer_start = std::min(std::find(*buffer_start, buffer_end, '\n'),
+                             std::find(*buffer_start, buffer_end, ','));
+  } else {
+    *buffer_start = std::find(*buffer_start, buffer_end, delim);
+  }
+  if (*buffer_start == buffer_end) return false;
+  *((*buffer_start)++) = 0;  // null terminate the number and advance to next
   return true;
 }
 template <typename T>
@@ -38,23 +49,73 @@ struct AsciiParser {
   AsciiParser(char* buffer, int size)
       : buffer_cur_(buffer), buffer_end_(buffer + size) {}
 
+  /// Checks that there is nothing left to parse in this string
+  bool checkDone() const { return (buffer_cur_ == buffer_end_); }
+
+  /// Asserts that next character matches argument
   bool checkChar(char expected) {
     return (buffer_cur_ != buffer_end_) && ((buffer_cur_++)[0] == expected);
   }
-
+  /// Reads the next character
   bool getChar(char* ret) {
     if (buffer_cur_ == buffer_end_) return false;
     *ret = (buffer_cur_++)[0];
     return true;
   }
+  /// If the character matches, then advance to next.  Else, do nothing
+  bool advanceOnMatchChar(char expected) {
+    if (buffer_cur_ == buffer_end_) return false;
+    if (*buffer_cur_ == expected) buffer_cur_++;
+    return true;
+  }
 
+  /// Parses an int, ending in delimiter
   template <typename T = int>
   bool parseInt(char delimiter, T* ret) {
     return human_serial::parseInt(&buffer_cur_, buffer_end_, delimiter, ret);
   }
+  /// Parses an int, ending in either ',' or '\n'
+  template <typename T = int>
+  bool parseInt(T* ret) {
+    return human_serial::parseInt(&buffer_cur_, buffer_end_, '\255', ret);
+  }
 
+  /// Parses a float, ending in delimiter
   template <typename T = float>
   bool parseFloat(char delimiter, T* ret) {
     return human_serial::parseFloat(&buffer_cur_, buffer_end_, delimiter, ret);
+  }
+  /// Parses a float, ending in ',' or '\n'
+  template <typename T = float>
+  bool parseFloat(T* ret) {
+    return human_serial::parseFloat(&buffer_cur_, buffer_end_, '\255', ret);
+  }
+
+  bool parse() { return checkDone(); }
+#define COMMA ,
+#define ENABLE_IF(expr) std::enable_if_t<expr, bool> = true
+  template <typename T, ENABLE_IF(std::is_same<char COMMA T>::value)>
+  bool parse(T* ret, char delimiter = '\n') {
+    return getChar(ret) && (delimiter == ',' ? advanceOnMatchChar(delimiter)
+                                             : checkChar(delimiter));
+  }
+  template <typename T, ENABLE_IF(std::is_integral<T>::value),
+            ENABLE_IF(!std::is_same<char COMMA T>::value)>
+  bool parse(T* ret, char delimiter = '\n') {
+    return parseInt(delimiter, ret);
+  }
+  template <typename T, ENABLE_IF(std::is_floating_point<T>::value)>
+  bool parse(T* ret, char delimiter = '\n') {
+    return parseFloat(delimiter, ret);
+  }
+  template <typename T1, typename... T>
+  bool parse(T1* ret, T*... otherRets) {
+    return parse(ret, ',') && parse(otherRets...);
+  }
+
+  template <typename... T>
+  bool peek(T*... ret) {
+    AsciiParser parser(*this);
+    return parser.parse(ret...);
   }
 };
---
+++ Firmware/gerry09_iLQR_t4/src/communication/ascii_parser_test.cpp
@@ -0,0 +1,66 @@
+#ifndef ARDUINO
+
+#include <algorithm>
+#include "ascii_parser.h"
+
+#include "../../CppUnitLite/TestHarness.h"
+
+char begin[] = "ca1.2,9,n\n";
+const AsciiParser parser_(begin, 10);
+char c, a, n;
+float f;
+int i, i2;
+
+TEST(ascii_parser, test_manual) {
+  AsciiParser parser = parser_;
+
+  CHECK(parser.checkChar('c'));
+  CHECK(parser.getChar(&a));
+  CHECK_EQUAL('a', a);
+  CHECK(parser.parseFloat(&f));
+  CHECK_EQUAL(1.2, f);
+  CHECK(parser.parseInt(&i));
+  CHECK_EQUAL(9, i);
+  CHECK(parser.checkChar('n'));
+  CHECK(parser.checkDone());
+}
+
+TEST(AsciiParser, test_template_success) {
+  AsciiParser parser = parser_;
+  CHECK(parser.checkChar('c'))
+  CHECK(parser.parse(&a, &f, &i, &n));
+  CHECK_EQUAL('a', a)
+  CHECK_EQUAL(1.2, f)
+  CHECK_EQUAL(9, i)
+  CHECK_EQUAL('n', n)
+}
+TEST(AsciiParser, test_template_int_as_char_fail) {
+  AsciiParser parser = parser_;
+  CHECK(parser.checkChar('c'))
+  CHECK(!parser.parse(&i2, &f, &i, &n));
+}
+TEST(AsciiParser, test_template_premature_fail) {
+  AsciiParser parser = parser_;
+  CHECK(parser.checkChar('c'))
+  CHECK(!parser.parse(&a, &f, &i));
+}
+TEST(AsciiParser, test_template_extra_char) {
+  AsciiParser parser = parser_;
+  CHECK(parser.checkChar('c'))
+  CHECK(!parser.parse(&a, &f, &i, &n, &i2));
+}
+
+TEST(AsciiParser, test_peek) {
+  AsciiParser parser = parser_;
+  CHECK(parser.checkChar('c'))
+  CHECK(!parser.peek(&a, &f, &i, &n, &i2));
+  CHECK(!parser.peek(&a, &f, &i));
+  CHECK(parser.parse(&a, &f, &i, &n));
+}
+
+int main() {
+  TestResult tr;
+  return TestRegistry::runAllTests(tr);
+}
+
+#endif
