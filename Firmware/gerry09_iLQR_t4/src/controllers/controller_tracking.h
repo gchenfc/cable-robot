85b824134121a88c3eedfb20d44ce14352327c18
Gerry Chen <gerry.chen2015@gmail.com>
2022-11-17 07:58:55 -0500

    Refactor trajectory interpolation/retiming There might be a small bug where the controller gets stuck between 2 values at the beginning of a trajectory, but I'm not sure.

--- Firmware/gerry09_iLQR_t4/src/controllers/controller_tracking.h
+++ Firmware/gerry09_iLQR_t4/src/controllers/controller_tracking.h
@@ -12,86 +12,61 @@ static constexpr float kSpeed = 0.1;
 static constexpr float kAccel = 1.0;
 static constexpr size_t kUpdateInterval_ms = 1;
 
-// class TrajectoryManager {
-//  private:
+/// @brief TrajectoryManager handles the mathematical aspects of interpolating
+/// and retiming a trajectory.
+class TrajectoryManager {
+ private:
+  using Vector2 = std::pair<float, float>;
+  CircularBuffer<Vector2, 1000> setpoints_;
+  float max_speed_, max_accel_;
+  float speed_;
+  Vector2 x_;
 
-// }
+ public:
+  TrajectoryManager(float max_speed, float max_accel, Vector2 current_xhat)
+      : max_speed_(max_speed), max_accel_(max_accel), speed_(0.0) {
+    reset(current_xhat);
+  }
 
+  void reset(Vector2 current_xhat) {
+    setpoints_.clear();
+    speed_ = 0;
+    x_ = current_xhat;
+    pushSetpoint(x_);
+  }
+
+  void setMaxSpeed(float max_speed) { max_speed_ = max_speed; }
+  void setMaxAccel(float max_accel) { max_accel_ = max_accel; }
+  bool pushSetpoint(Vector2 x);
+  bool dropSetpoint();
+  bool translate(float dx, float dy);
+
+  const Vector2& getCurrentSetpoint() const { return x_; }
+  void updateSpeed();     // Updates `speed_`
+  void updateSetpoint();  // Updates `x_`
+
+ protected:
+  /// @brief Returns the path length from the current position to the next
+  /// direction change
+  /// @param max If the path length is greater than max, then this function will
+  /// just return max.
+  float calcPathLength(float max = std::numeric_limits<float>::max()) const;
+};
+
+/// @brief ControllerTracking takes setpoints/waypoints as Serial input and
+/// updates desPos and desVel accordingly
 class ControllerTracking : public ControllerSimple {
  public:
   ControllerTracking(const StateEstimatorInterface* state_estimator)
       : ControllerSimple(state_estimator),
         cur_(kWidth / 2, kHeight / 2),
-        setpoint_() {
-    setpoint_.push(cur_);
-  }
-
-  float calcDistance() const {
-    static constexpr float DOT_THRESH = cosf(30 * kTau / 360);
-    float dist = 0;
-    auto tmp_setpoints = setpoint_.view();
-    Vector2 prev_setpoint = cur_, setpoint;
-    bool skip_next = true;
-    float prev_dx[2] = {0, 0};
-    while (tmp_setpoints.pop(setpoint)) {
-      float dx[2] = {setpoint.first - prev_setpoint.first,
-                     setpoint.second - prev_setpoint.second};
-      float to_add = normalize(dx);
-      if (!skip_next && (dot(dx, prev_dx) < DOT_THRESH)) {
-        break;  // treat this essentially as a "new stroke"
-      }
-      dist += to_add;
-      prev_setpoint = setpoint;
-      std::memcpy(prev_dx, dx, sizeof(dx));
-      // skip_next = to_add < 0.01;
-      skip_next = skip_next && (to_add < 0.01);
-    }
-    return dist;
-  }
-  bool pushSetpoint(Vector2 setpoint) {
-    clamp(&setpoint.first, limit_left_, kWidth - limit_right_);
-    clamp(&setpoint.second, limit_down_, kHeight - limit_up_);
-    if (setpoint_.empty()) {
-      setpoint_distance_ = 0;
-    } else {
-      const auto& last = setpoint_.back();
-      float dx[2] = {setpoint.first - last.first,
-                     setpoint.second - last.second};
-      float d = norm(dx);
-      if (d < 0.001) return false;
-      setpoint_distance_ += norm(dx);
-    }
-    return setpoint_.push(setpoint);
-  }
-  bool dropSetpoint() {
-    Vector2 setpoint;
-    if (!setpoint_.pop(setpoint)) return false;
-    if (setpoint_.empty()) {
-      setpoint_distance_ = 0;
-    } else {
-      const auto& first = setpoint_.front();
-      float dx[2] = {setpoint.first - first.first,
-                     setpoint.second - first.second};
-      setpoint_distance_ -= norm(dx);
-    }
-    return true;
-  }
-  bool translateSetpoint(float dx, float dy) {
-    if (setpoint_.empty()) return false;
-    setpoint_.front().first += dx;
-    setpoint_.front().second += dy;
-    return true;
-  }
-
-  void updateSetpoint();
+        trajectory_(kSpeed, kAccel, cur_) {}
 
   bool readSerial(AsciiParser parser, Stream& serialOut) override;
 
  protected:
-  float speed = kSpeed, accel_ = kAccel, cur_speed_ = 0;
   Vector2 cur_;
-  CircularBuffer<Vector2, 1000> setpoint_;
-  float setpoint_distance_;
+  TrajectoryManager trajectory_;
   float limit_left_ = 0.2, limit_right_ = 0.2, limit_up_ = 0.2,
         limit_down_ = 0.2;
   Metro setpointUpdateTimer_{kUpdateInterval_ms};
@@ -100,22 +75,12 @@ class ControllerTracking : public ControllerSimple {
   void myUpdate() override {
     if ((state_ == HOLD_TRAJ_BEGIN) ||
         ((state_ == RUNNING_TRAJ) && (prev_state_ != RUNNING_TRAJ))) {
-      cur_ = state_estimator_->posEst();
-      setpoint_.clear();
-      setpoint_distance_ = 0;
-      pushSetpoint(cur_);
+      trajectory_.reset(state_estimator_->posEst());
     }
-    // if (state_ == RUNNING_TRAJ) {
-    //   // For safety reasons, snap the desired position to current estimated
-    //   // position just in case.
-    //   const Vector2& x = state_estimator_->posEst();
-    //   towards(1e-3 * kUpdateInterval_ms * speed,                //
-    //           x.first, x.second,           //
-    //           cur_.first, cur_.second,     //
-    //           &cur_.first, &cur_.second);  //
-    // }
     if (setpointUpdateTimer_.check()) {
-      updateSetpoint();
+      trajectory_.updateSpeed();
+      trajectory_.updateSetpoint();
+      cur_ = trajectory_.getCurrentSetpoint();
       clamp(&cur_.first, limit_left_, kWidth - limit_right_);
       clamp(&cur_.second, limit_down_, kHeight - limit_up_);
     }
@@ -136,29 +101,33 @@ bool ControllerTracking::readSerial(AsciiParser parser, Stream& serialOut) {
     case 'a':
       UNWRAP_PARSE_CHECK(, parser.parseFloat(',', &setpoint.first));
       UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &setpoint.second));
-      pushSetpoint(setpoint);  // returns false if full
+      clamp(&setpoint.first, limit_left_, kWidth - limit_right_);
+      clamp(&setpoint.second, limit_down_, kHeight - limit_up_);
+      trajectory_.pushSetpoint(setpoint);  // returns false if full
       break;
     case 'r':
       UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
-      translateSetpoint(amt, 0);
+      trajectory_.translate(amt, 0);
       break;
     case 'l':
       UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
-      translateSetpoint(-amt, 0);
+      trajectory_.translate(-amt, 0);
       break;
     case 'u':
       UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
-      translateSetpoint(0, amt);
+      trajectory_.translate(0, amt);
       break;
     case 'd':
       UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
-      translateSetpoint(0, -amt);
+      trajectory_.translate(0, -amt);
       break;
     case 's':
-      UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &speed));
+      UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
+      trajectory_.setMaxSpeed(amt);
       return true;
     case 'A':
-      UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &accel_));
+      UNWRAP_PARSE_CHECK(, parser.parseFloat('\n', &amt));
+      trajectory_.setMaxAccel(amt);
       return true;
     case 'L': {  // limits
       UNWRAP_PARSE_CHECK(char dir, parser.getChar(&dir));
@@ -186,49 +155,85 @@ bool ControllerTracking::readSerial(AsciiParser parser, Stream& serialOut) {
   return true;
 }
 
-void ControllerTracking::updateSetpoint() {
-  // first update speed (calculate total path length)
-  // float distance_to_final_setpoint = 0;
-  // {  // calculate distance to final setpoint
-  //   Vector2 first_setpoint = setpoint_.front();
-  //   float dx[2] = {first_setpoint.first - cur_.first,
-  //                  first_setpoint.second - cur_.second};
-  //   distance_to_final_setpoint = setpoint_distance_ + norm(dx);
-  // }
-  // distance_to_final_setpoint = max(0.0f, distance_to_final_setpoint);
-  float distance_to_final_setpoint = calcDistance();
+/******************************************************************************/
+
+bool TrajectoryManager::pushSetpoint(Vector2 setpoint) {
+  const auto& last = setpoints_.back();
+  float dx[2] = {setpoint.first - last.first, setpoint.second - last.second};
+  float d2 = norm2(dx);
+  if (d2 < (0.001 * 0.001)) return false;
+  return setpoints_.push(setpoint);
+}
+bool TrajectoryManager::dropSetpoint() { return setpoints_.drop(); }
+bool TrajectoryManager::translate(float dx, float dy) {
+  pushSetpoint(Vector2{x_.first + dx, x_.second + dy});
+  return true;
+}
+
+void TrajectoryManager::updateSpeed() {
   // Update speed
-  float min_new_speed = cur_speed_ - 1e-3 * kUpdateInterval_ms * accel_;
-  // float min_new_speed = 0.0f;
-  float max_new_speed = cur_speed_ + 1e-3 * kUpdateInterval_ms * accel_;
-  // cur_speed_ = sqrt(2 * accel_ * max(0, distance_to_final_setpoint - 0.1));
-  cur_speed_ = sqrt(2 * accel_ * distance_to_final_setpoint);
-  clamp(&cur_speed_, min_new_speed, min(max_new_speed, speed));
+  float distance_max_velocity = 0.5 * max_speed_ * max_speed_ / max_accel_;
+  float distance_to_final_setpoint = calcPathLength(distance_max_velocity);
+  float min_new_speed = speed_ - 1e-3 * kUpdateInterval_ms * max_accel_;
+  float max_new_speed = speed_ + 1e-3 * kUpdateInterval_ms * max_accel_;
+  speed_ = sqrt(2 * max_accel_ * distance_to_final_setpoint);
+  clamp(&speed_, min_new_speed, min(max_new_speed, max_speed_));
+
+  // Debug print
   if ((millis() % 100) == 0) {
     if (Serial.availableForWrite() > 400) {
       Serial.printf(
           "dist: %.4f,    cur speed: %.4f,   min: %.4f,   max: %.4f,   cur "
           "accel: %.4f\n",  //
-          distance_to_final_setpoint, cur_speed_, min_new_speed, max_new_speed,
-          accel_);
+          distance_to_final_setpoint, speed_, min_new_speed, max_new_speed,
+          max_accel_);
     }
   }
+}
 
-  // now update setpoint
-  float dist_to_go = 1e-3 * kUpdateInterval_ms * cur_speed_;
+void TrajectoryManager::updateSetpoint() {
+  float dist_to_go = 1e-3 * kUpdateInterval_ms * speed_;
   while (dist_to_go > 0) {
-    const Vector2& setpoint = setpoint_.front();
+    const Vector2& setpoint = setpoints_.front();
     float d2 = towards(dist_to_go,                       //
-                       cur_.first, cur_.second,          // current
+                       x_.first, x_.second,              // current
                        setpoint.first, setpoint.second,  // target
-                       &cur_.first, &cur_.second);       // new
+                       &x_.first, &x_.second);           // new
     dist_to_go -= sqrt(d2);
-    if (dist_to_go > 0) {          // reached setpoint
-      if (setpoint_.size() > 1) {  // always keep at least 1 setpoint
-        dropSetpoint();            // continue to next setpoint
+    if (dist_to_go > 0) {           // reached setpoint
+      if (setpoints_.size() > 1) {  // always keep at least 1 setpoint
+        dropSetpoint();             // continue to next setpoint
       } else {
         break;  // no more setpoints left - continue to current
       }
     }
   }
+}
+
+float TrajectoryManager::calcPathLength(float max) const {
+  static constexpr float DOT_THRESH = cosf(30 * kTau / 360);
+  // Initialize stuff
+  float dist = 0;
+  auto tmp_setpoints = setpoints_.view();  // make a copy to iterate
+  Vector2 prev_setpoint = x_, setpoint;
+  bool skip_next = true;
+  float prev_dx[2] = {0, 0};
+  // Loop through all the setpoints, accumulating the distances between them
+  while (tmp_setpoints.pop(setpoint)) {
+    float dx[2] = {setpoint.first - prev_setpoint.first,
+                   setpoint.second - prev_setpoint.second};
+    float to_add = normalize(dx);
+    if (!skip_next && (dot(dx, prev_dx) < DOT_THRESH)) {
+      break;  // If we hit a direction change, we want to come to a full stop
+    }
+    dist += to_add;
+    if (dist >= max) {
+      return max;
+    }
+    // update "previous" variables
+    prev_setpoint = setpoint;
+    std::memcpy(prev_dx, dx, sizeof(dx));
+    skip_next = skip_next && (to_add < 0.01);
+  }
+  return dist;
 }
